a9aeaeb3e915240bbb6e892aa0a491050c4b6e98
Michael Steven Rodriguez Lozano <msrodriguez@procibernetica.com>
2025-06-18 11:25:35 -0500

    update files

--- vpc_dev_main.tf
+++
@@ -1,9 +0,0 @@
-module "vpc" {
-  source  = "app.terraform.io/pcb/vpc/gcp"
-  version = "1.0.2"
-  project   = "tst"
-  region      = "tst"
-  zone        = "tst"
-  vpc_name    = "tst"
-  subnet_name = "tst"
-}
